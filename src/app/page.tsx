3278d4b9f1cd50cb4850533c9cb85df9df1c0989
Jacob Silverstone <jsilverstone@gmail.com>
2026-08-05 18:02:46 -0400

    Make logo dramatically larger

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
 <Image
   src="/logo.png"
   alt="Rewired Ketamine"
-  width={800}
-  height={240}
-  className="h-32 md:h-40 w-auto"
+  width={8000}
+  height={2400}
+  className="h-[2400px] w-auto"
   priority
 />
           </Link>
