d99c1c51e02ac430d2589a6254a34f5ee29c5954
Jacob Silverstone <jsilverstone@gmail.com>
2026-09-08 13:38:36 -0400

    Redirect /free-consultation and /the-experience

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
+  source: "/free-consultation",
+  destination: "/#contact",
+  permanent: true,
+},
+{
+  source: "/the-experience",
+  destination: "/",
+  permanent: true,
+},
+      {
   source: "/locations",
   destination: "/location",
   permanent: true,
